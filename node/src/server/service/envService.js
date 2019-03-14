49126f177b8b982bdc3dc08dfd49d1e60b02a168
EC2 Default User <ec2-user@ip-172-31-2-219.us-east-2.compute.internal>
2019-03-14 18:41:17 +0000

    cleaned up package scripts and project details

--- node/src/server/service/envService.js
+++ node/src/server/service/envService.js
@@ -1,20 +1,19 @@
-require('dotenv').config()
+require('dotenv').config();
 import dotenv from "dotenv";
-
     export function importEnvVars () {
         require('dotenv').config();
         let result = dotenv.config();
         if (result.error) { throw result.error }
         const { parsed: envs } = result;
         return envs;
-    };
+    }
     export function stringToParsedEnvObj (stringToParse) {
         const dotenv = require('dotenv');
         const buf = Buffer.from(stringToParse);
         const config = dotenv.parse(buf);
         let result = config;
         return result;
-    };
+    }
     export function debugStringToEnvObj (stringToParse) {
         const dotenv = require('dotenv');
         const buf = Buffer.from(stringToParse);
@@ -22,6 +21,6 @@ import dotenv from "dotenv";
         const config = dotenv.parse(buf, opt);
         let result = config;
         return result;
-    };
+    }
 const ENVS = importEnvVars();
 export default {ENVS};
