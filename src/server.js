94ec4df43315a3779f02bfe7ed2aaab2ad935944
EC2 Default User <ec2-user@ip-172-31-17-48.us-east-2.compute.internal>
2019-04-01 12:35:22 +0000

    pulled server file changes

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const dotenv = require('dotenv').config();
 import path         from 'path';
 import express      from 'express';
 import cookieParser from 'cookie-parser';
@@ -6,20 +7,7 @@ import logger       from "./service/winston-logger";
 import morgan       from "morgan";
 
 import MongoService from "./service/mongoService";
-import index from "./view/index.js";
-
-/**
- * Global Vendor Tooling
- * TODO
- */
-
-/**
- * Trust Proxy
- * TODO
- */
- 
- // crude
-const SERVER_PORT = 8080;
+import render from "./view/render.js";
 
 const app = express();
 
@@ -34,55 +22,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 /**
- * Register Cookie Authentication
- * TODO
- */
-
-
-/**
  * Register MongoDB API
  * 
  * Receives and stores data from Arduino MK2
  * Retrieves data from mongo database
- * TO DO Refactor to new folder
  */
 app.get('/record/:tmp/:moi/:lum',function(req,res,next){
-  let mongoService = new MongoService("0.0.0.0","27017","admin","4dmInP4ssw0rd");
+  let mongoService = new MongoService("mongodb","27017","admin","4dmInP4ssw0rd");
   mongoService.setRecord(req.params.tmp,req.params.moi,req.params.lum);
   mongoService.insertReading("mydatabase","records");
-  next();
 });
 app.get('/record',function(req,res,next){
-  let mongoService = new MongoService("0.0.0.0","27017","admin","4dmInP4ssw0rd");
+  let mongoService = new MongoService("mongodb","27017","admin","4dmInP4ssw0rd");
   mongoService.getReadings("mydatabase","records");
-  next();
 });
 
 /**
  * Register Server Side Rendering
- * TODO
  */
 app.get('*', function(req, res) {
-  index(req,res);
-    // Enables critical path CSS rendering
-    // https://github.com/kriasoft/isomorphic-style-loader
-    
-    // Universal HTTP client
-    
-    // Global (context) variables that can be easily accessed from any React component
-    // https://facebook.github.io/react/docs/context.html
+  render(req,res);
 });
-
-/**
- * Error Handling
- * Seperation from listener
- * TODO
- */
  
 /**
  * Launch Server
  */
-app.listen(SERVER_PORT, function(err,req,res) {
+app.listen(process.env.SERVER_PORT, function(err,req,res) {
   // Error logger configurations
 	if (err) {
     res.locals.message = err.message;
@@ -91,12 +56,8 @@ app.listen(SERVER_PORT, function(err,req,res) {
     res.status(err.status || 500);
     res.render('error');
 	}
-	logger.info(`APP is now running on port ${SERVER_PORT}`);
+
+  logger.info('Listening on', process.env.SERVER_PORT);
 });
 
-/**
- * Hot Module Replacement
- * TODO
- */
- 
- export default app;
+export default app;
