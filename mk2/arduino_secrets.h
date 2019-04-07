68391bb4fe30ccfd6f1e3390f8f71c023adfcdf7
Jesse Roy-Cote <jesseroyc@gmail.com>
2019-04-07 21:46:40 +0000

    Light relays implemented

--- mk2/arduino_secrets.h
+++ mk2/arduino_secrets.h
@@ -1,6 +1,4 @@
-#define SECRET_SSID "Tobiko_2G"
-#define SECRET_PASS "4035122345"
-//#define SECRET_SSID "SM-G950W0677"
-//#define SECRET_PASS "huhw8708"
+//#define SECRET_SSID "example"
+//#define SECRET_PASS "example"
 #define SECRET_SERV "www.team-twenty-one.com"
 #define SAMPLE_TIME_SECS 1
