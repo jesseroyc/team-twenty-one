a1edc1add7e1dd9002d1633fdfb710e5c76dbca6
Jesse Roy-Cote <jesseroyc@gmail.com>
2019-04-09 16:10:20 +0000

    Setup folders for remote deployment outside of Cloud9

---
+++ dist/precache-manifest.aba966b86ee6fb4eae7f2491b1875143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6e126c67a4b7599cc55e",
+    "url": "/static/js/main.a568a0b8.chunk.js"
+  },
+  {
+    "revision": "77ced82afcbdeb6115f3",
+    "url": "/static/js/2.0af78763.chunk.js"
+  },
+  {
+    "revision": "6e126c67a4b7599cc55e",
+    "url": "/static/css/main.dfda653b.chunk.css"
+  },
+  {
+    "revision": "da82edb9f208d30688bfc43a559d2d16",
+    "url": "/index.html"
+  }
+];
